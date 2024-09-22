c08ae153d963d7864ac9cf5ee5b9075945253f15
northagalvankellieb3 <northagalvankellieb3@hotmail.com>
2024-09-22 14:28:43 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("3997a3ec8aa2157702dc7fa298dcb37c82bad54df2dc0d972bc5a5f19f3e9d4f", "3997a3ec8aa2157702dc7fa298dcb37c82bad54df2dc0d972bc5a5f19f3e9d4f") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
